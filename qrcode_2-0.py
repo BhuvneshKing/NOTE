b90a9941b5735e2a55ff851c77e049e5fc339fd7
BhuvneshKing <bhuvneshverma2904@gmail.com>
2023-02-15 03:51:37 +0530

    working

--- qrcode_2-0.py
+++ qrcode_2-0.py
@@ -14,3 +14,27 @@ img = qr.make_image(fill_color="black", back_color="white")
 
 # Save the image as a PNG file
 img.save("qr_code.png")
+
+
+from tkinter import *
+from PIL import ImageTk, Image
+import os
+
+with Image.open("qr_code.png") as img:
+    width, height = img.size
+    global Width,Height
+    Width=(width)
+    Height=(height)
+
+root = Tk()
+
+def delete_file_on_close():
+    os.remove("qr_code.png")
+    root.destroy()
+
+img = Image.open("qr_code.png")
+photo = ImageTk.PhotoImage(img)
+label = Label(root, image=photo, width=Width, height=Height)
+label.pack()
+root.protocol("WM_DELETE_WINDOW", delete_file_on_close)
+root.mainloop()
